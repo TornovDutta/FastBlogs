872374bf75d5dcfb4ea05f508fa0ad05d37a1502
TornovDutta <tornovdutta@gmail.com>
2026-03-21 18:04:03 +0530

    add config and database

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,10 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL:str="sqlite:///./database.db"
-    
-    class Config:
-        evn_file=".env"
-Setting=Settings()
+    DATABASE_URL: str = "sqlite:///./database.db"
 
+    model_config = {
+        "env_file": ".env"
+    }
+
+settings = Settings()
--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
-from app.core.config import Settings
-engine=create_engine(Settings.DATABASE_URL,connect_args={"check_same_thread":False})
+from app.core.config import settings
+
+
+
+
+engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal=sessionmaker(bind=engine,autoflush=True,autoCommit=True)
 Base=declarative_base()
